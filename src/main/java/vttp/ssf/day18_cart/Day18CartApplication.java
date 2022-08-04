5a7f089bbd955d494e5e7a36a616e4a1fa98ebf2
Yizhun <sim.yizhun@gmail.com>
2022-08-04 12:51:05 +0800

    master

---
+++ src/main/java/vttp/ssf/day18_cart/Day18CartApplication.java
@@ -0,0 +1,13 @@
+package vttp.ssf.day18_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day18CartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day18CartApplication.class, args);
+	}
+
+}
